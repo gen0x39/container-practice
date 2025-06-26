f3c287a4b14267d76115a119b1df66853b71b101
bqnq <shinji-kikuchi@pepabo.com>
2025-06-26 12:48:13 +0900

    fix: namespace

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000',  // ローカル開発用
-  OTLP_TRACE_EXPORTER_URL: 'http://backend:4318/v1/traces' // docker compose用
+  OTLP_TRACE_EXPORTER_URL: 'http://datadog-agent.monitoring.svc.cluster.local:4318/v1/traces'
 };
